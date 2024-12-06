fc815eda7e7cf68bee2450ab0ac9e02d076379a6
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-12-06 21:40:58 +0530

    Add files via upload

---
+++ LeetCodeProblems/MaxSlidingWindow.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MaxSlidingWindow {
+
+	public int[] slidingWindow(int[] nums , int k) {
+		int n = nums.length;
+		int[] r = new int[n-k+1];
+		int ri =0;
+		Deque<Integer>  q = new ArrayDeque<>();
+		for(int i=0;i<nums.length;i++) {
+			while(!q.isEmpty() && q.peek()<i-k+1) {
+				q.poll();
+			}
+			while(!q.isEmpty() && nums[q.peekLast()]<nums[i]) {
+				q.pollLast();
+			}
+			q.offer(i);
+			if(i>= k-1) {
+				r[ri++] =  nums[q.peek()];
+			}
+		}
+		return r;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
