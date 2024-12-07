df6218d5bd325975a80929fb9c7c62cd30bde079
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-12-07 19:02:14 +0530

    Add files via upload

---
+++ HashMapAssignment/Assignment2.java
@@ -0,0 +1,31 @@
+package HashMapProblems;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Assignment2 {
+	//TC = O(nlogn)
+	//Sc = O(n)
+	public static void main(String[] args) {
+		//Step -1
+		//Input the no. of key value pairs
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of key-value pairs :");
+		int n = scanner.nextInt();
+		//Step -2
+		//Create A Treemap 
+		TreeMap<String, Integer> map = new TreeMap<>();
+		//Step -3
+		//Insert the key value pairs and insert them in the Treemap
+		System.out.println("Insert the key values pairs : ");
+		for(int i=0;i<n;i++) {
+			int key = scanner.nextInt();
+			String value = scanner.nextLine();
+			
+			map.put(value, key);
+		}
+		//Step -4
+		//print the map
+		System.out.println("The map is : "+map);
+	}
+}
