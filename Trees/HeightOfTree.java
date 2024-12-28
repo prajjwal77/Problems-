78b69c46bb27c7d819bd70024dacf123448b0bee
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-12-28 20:29:51 +0530

    Add files via upload

---
+++ Trees/HeightOfTree.java
@@ -0,0 +1,34 @@
+package Trees;
+
+class Node {
+	int val;
+	Node left = null;
+	Node right = null;
+
+	Node() {
+	}
+
+	Node(int val) {
+		this.val = val;
+	}
+}
+
+public class HeightOfTree {
+	public static int height(Node root) {
+		if (root == null)
+			return 0;
+
+		return 1 + Math.max(height(root.left), height(root.right));
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		System.out.println("Height of tree is : "+height(root));
+	}
+}
